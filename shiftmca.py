55e71f07183ccff05c28238999b4f04feaad106b
Agaricus <agaricusb@yahoo.com>
2012-09-29 17:03:46 -0700

    Repair mca. This increases file size by +8m each run

--- shiftmca.py
+++ shiftmca.py
@@ -1,9 +1,17 @@
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
 import sys
 sys.path.append("..")
 import pymclevel
 
 root = "../bukkit/SERVER-beta-firstworld-anvil1.2.5/New World/region/renamed/"
-filename = root + "r.17.0.mca"
+filename = root + "r.20.0.mca"
 
-region = pymclevel.infiniteworld.MCRegionFile(filename, (17, 0))
+region = pymclevel.infiniteworld.MCRegionFile(filename, (20, 0))
+#region.extractAllChunks("/tmp/2")
 print region
+region.repair()
+region.close()
+
